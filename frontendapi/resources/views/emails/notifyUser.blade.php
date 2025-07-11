6027d79fc809017cd2c6a1c35f2fa3c1ecfffc73
200kg-bench <chjoel12@gmail.com>
2025-07-11 09:22:17 +0700

    notify user

---
+++ frontendapi/resources/views/emails/notifyUser.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your OTP Code</title>
+</head>
+<body>
+    <p>Dear user,</p>
+    <p>Your account has been reviewed and approved</p>
+    <p>You can now access our website</p>
+</body>
+</html>
